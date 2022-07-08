6f3027c52a5c19826863ee94ca50b7e77984e192
YoungeonLee <youngl@bu.edu>
2022-07-08 11:35:38 +0900

    add create_todo, get_todo, and get_todos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from datetime import datetime
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,6 +15,51 @@ def my_string():
 @app.route('/user/<username>')
 def profile(username):
     return f'hello {username}'
+
+todos = [
+    {
+        'id': 0,
+        #                 year month day hour minute
+        'deadline': datetime(2022, 7, 15, 12, 16),
+        'name': 'name 0',
+        'description': 'description 0'
+    },
+    {
+        'id': 1,
+        'deadline': datetime(2022, 7, 15, 12, 16),
+        'name': 'name 1',
+        'description': 'description 1'
+    },
+    {
+        'id': 2,
+        'deadline': datetime(2022, 7, 15, 12, 16),
+        'name': 'name 2',
+        'description': 'description 2'
+    },
+]
+
+id_tracker = 3
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def create_todo():
+    global id_tracker
+    todo = {
+        'id': id_tracker,
+        'deadline': datetime.fromtimestamp(int(request.form['deadline_unix_timestamp'])),
+        'name': request.form['name'],
+        'description': request.form['description'],
+    }
+    id_tracker += 1
+    todos.append(todo)
+    return jsonify(todo)
+
+@app.route('/todo/<id>')
+def get_todo(id):
+    return jsonify(todos[int(id)])
 
 
 # 0.0.0.0 accessible by anyone in network
